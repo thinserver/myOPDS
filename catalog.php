78295b33d00cae6f9c45357a233105c425a4b1d0
Matthias <mail@matthiasbock.net>
2012-06-10 18:45:24 +0200

    index.php works with FBReader

---
+++ catalog.php
@@ -0,0 +1,26 @@
+<?php
+
+header('Content-Type: application/atom+xml');
+print '<?xml version="1.0" encoding="UTF-8"?>
+<feed xmlns="http://www.w3.org/2005/Atom"
+      xmlns:dc="http://purl.org/dc/terms/"
+      xmlns:opds="http://opds-spec.org/2010/catalog">
+
+  <id>Catalog</id>
+  <title>Catalog</title>
+  <author>
+    <name>Matthias Bock</name>
+  </author>
+
+';
+	$folder = 'files';
+
+	$files = scandir($folder);
+	foreach ($files as $file) {
+		if (strpos($file, '.xml') > -1) {
+			$name = $folder.'/'.$file;
+			readfile($name);
+			}
+		}
+?>
+</feed>
--- index.php
+++ index.php
@@ -1,14 +1,3 @@
 <?php
-	readfile('header.xml');
-
-	$folder = 'files';
-
-	$files = scandir($folder);
-	foreach ($files as $file) {
-		if (strpos($file, '.xml') > -1) {
-			$name = $folder.'/'.$file;
-			readfile($name);
-			}
-		}
+	readfile('start.xml');
 ?>
-</feed>
