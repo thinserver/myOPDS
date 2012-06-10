ada0a2ed274f98ac362404844d8c792f6ce15b98
Matthias <mail@matthiasbock.net>
2012-06-10 19:25:37 +0200

    Icon

--- epub.php
+++ epub.php
@@ -5,4 +5,6 @@
 
 # run calibre: ebook-convert x.pdf x.ebook
 
+readfile($epubs.'/'.'Understanding the role of host hemocytes in a squid Vibrio symbiosis using transcriptomics and proteomics.epub');
+
 ?>
--- thumbnail.php
+++ thumbnail.php
@@ -1,9 +1,6 @@
 <?php
 
 	$thumbnails = "thumbnails";
-	$files = "files";
-
-	$md5 = md5_file($files.'/'.$_GET['filename']);
-	readfile($thumbnails.'/'.$md5.'.png');
+	readfile($thumbnails.'/'.$_GET['filename'][:-4].'.png');
 
 ?>
